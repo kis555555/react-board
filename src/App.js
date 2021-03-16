bf3dea2c9a67f1353dd3ba2f2d0aa3d0a66666f0
kis555555 <kis555555@naver.com>
2021-03-16 21:42:48 +0900

    sign up success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Main from "./main";
+import Signup from "./account/signup";
+import { BrowserRouter, Route } from 'react-router-dom'; //React-Router import
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render()
+  {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Route path="/" exact component={Main}/>
+          <Route path="/signup" exact component={Signup}/>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 export default App;
